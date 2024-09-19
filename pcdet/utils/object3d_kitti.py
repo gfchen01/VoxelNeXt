b6d0c534fc19d2256884a3df6c3cd918e77a9a61
Guofei Chen <guofei@cmu.edu>
2024-09-19 11:22:17 -0400

    Minor changes to fit v4 dataset

--- pcdet/utils/object3d_kitti.py
+++ pcdet/utils/object3d_kitti.py
@@ -17,8 +17,8 @@ def cls_type_to_id(cls_type):
         4: 'cabinet',
         5: 'desk',
         6: 'bed',
-        7: 'bench',
-        8: 'refridgerator',
+        # 7: 'bench',
+        7: 'refridgerator',
         }
     if cls_type not in type_to_id.keys():
         return -1
